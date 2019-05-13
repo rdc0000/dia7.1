2b95107bcf799833430d8b10225b3a25b90fe17a
rdc0000 <rdc.159@hotmail.com>
2019-05-13 12:41:22 -0500

    Initial commit

---
+++ Interface1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dia7._1
+{
+    interface IVehiculo
+    {
+        void DefinirVenta(string cad);
+        void DarBaja(bool baja);
+            
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace dia7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //si es un auto cuyo añ es inferior a 1700 se debe dar de   baja
+            //si es una moto ingerior a 1900 se debe dar de baja
+            // el metodo debe dovolver un valor boleano si se puede dar de baja
+        }
+    }
+}
